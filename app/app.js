14332d4ac1604e94cc9c96aa959a26ebae1a7706
Lautaro Ezequiel Rinaldi <lautaroezequielrinaldi@yahoo.com.ar>
2022-05-15 00:50:33 -0300

    Se configura correctamente nginx y se empieza con los escenarios de las pruebas

--- app/app.js
+++ app/app.js
@@ -1,22 +1,47 @@
 const express = require("express");
+const axios = require('axios');
 const app = express();
 
 app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
+
+app.get("/ping", (req, res) => {
   res.status(200).send("ping");
+});
+
+app.get("/bbox1", (req, res) => {
+    axios.get(`http://bbox:9090/`)
+        .then(result => {
+            res.send(`${result.data}`);
+        })
+        .catch(err => {
+            res.send(`${err.message}`);
+        });
+});
+
+app.get("/bbox2", (req, res) => {
+  axios.get(`http://bbox:9091/`)
+      .then(result => {
+          res.send(`${result.data}`);
+      })
+      .catch(err => {
+          res.send(`${err.message}`);
+      });
 });
 
 app.get("/delay", (req, res) => {
   setTimeout(()=>{
     res.status(200).send("delay");
   })
-})
+});
 
 app.get("/heavy", (req, res) => {
   let start = new Date();
   while(new Date() - start < 5000);
   res.status(200).send("heavy");
-})
+});
 
-app.listen(3500, () => {
-  console.log("Escuchando en", 3500);
-})
+app.listen(4000, () => {
+  console.log("Escuchando en", 4000);
+});
